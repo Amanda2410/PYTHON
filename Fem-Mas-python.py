9b98c7ba8d651bb153d53205929984094cb1dc39
Amanda2410 <142624930+Amanda2410@users.noreply.github.com>
2023-09-26 10:36:03 -0300

    Add files via upload

---
+++ Fem-Mas-python.py
@@ -0,0 +1,9 @@
+
+letra= (input("Informe uma letra: "))
+
+if (letra=="F") or (letra=="f"):
+   print("F-Feminino")
+elif (letra=="M") or (letra=="m"):
+    print("M-Masculino")
+else:
+    print("Sexo inválido")
